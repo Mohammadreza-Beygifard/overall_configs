245540711f063c496b3563db8f154f0f5944c68c
Mohammadreza Beygifard <mohammadreza.beygifard@stellantis.com>
2022-02-25 10:14:02 +0100

    Add nvim git ignore

---
+++ dotfiles/nvim/lua/user/treesitter.lua
@@ -0,0 +1,21 @@
+local status_ok, configs=pcall(require, "nvim-treesitter.configs")
+if not status_ok then
+        vim.notify("syntax-highlighting is not available!")
+        return
+end
+
+
+
+local configs = require("nvim-treesitter.configs")
+configs.setup {
+  ensure_installed = "maintained",
+  sync_install = false, 
+  ignore_install = { "" }, -- List of parsers to ignore installing
+  highlight = {
+    enable = true, -- false will disable the whole extension
+    disable = { "" }, -- list of language that will be disabled
+    additional_vim_regex_highlighting = true,
+
+  },
+  indent = { enable = true, disable = { "yaml" } },
+}
